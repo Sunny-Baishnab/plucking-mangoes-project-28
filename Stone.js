041c2b1555a4f2e716cd66f666fdb17dd4065439
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2020-11-02 15:45:03 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -1,13 +1,13 @@
 class Stone{
-    constructor(x,y){
+    constructor(x,y,radius){
         var options={
             isStatic:false,
             restitution:0,
             friction:1,
             density:1
         }
-        this.body=Bodies.circle(x,y,10,options);
-        this.radius=10;
+        this.body=Bodies.circle(x,y,radius,options);
+        this.r=radius;
         this.image=loadImage("stone.png");
         World.add(world,this.body);
     }
@@ -16,7 +16,7 @@ class Stone{
         push();
         translate(pos.x,pos.y);
         imageMode(RADIUS);
-        image(this.image,0,0,40,40);
+        image(this.image,0,0,this.r,this.r);
         pop();
     }
 }
